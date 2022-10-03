c6696bf83b5e58e93616c46b55ace03cb2232c40
Trey <burlints@mail.uc.edu>
2022-10-03 18:34:54 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 22;
+
+        if (age >= 21)
+        {
+            System.out.println("You said you are " + age + ", so you get a wrist band ");
+        }
+        else
+        {
+
+        }
+    }
+}
